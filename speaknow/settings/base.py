8ae5d66601f942210752d1f067f2cd3a0ac55fa5
vagrant <vagrant@precise64.(none)>
2015-09-01 22:38:23 +0000

    Added the static/ folder

--- speaknow/settings/base.py
+++ speaknow/settings/base.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'speaknow.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(),
 }
 
 # Internationalization
