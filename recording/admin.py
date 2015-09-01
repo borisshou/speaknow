9f22cc1a14ac7dad66b9c03a27a6be55c1df459d
vagrant <vagrant@precise64.(none)>
2015-09-01 22:15:22 +0000

    Revised admin page

--- recording/admin.py
+++ recording/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
+
+from .models import Recording, Comment
+from speaknow.models import User, Learner
+from .models import Recording, Comment
+
+
+
+class LearnerInline(admin.StackedInline):
+    model = Learner
+    can_delete = False
+
+# Define a new User admin
+class UserAdmin(UserAdmin):
+    inlines = (LearnerInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Recording)
+admin.site.register(Comment)
+
+
